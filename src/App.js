9a108117c6b43822e5fa9e894104ce95b6df7235
AbdelrahmanSE <abdelrahmanse@flexnebula.com>
2019-06-23 00:06:52 +0200

    Created Header Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
-import "./App.css";
+import "./App.module.scss";
+import Header from './components/Layout/Header/Header';
 
 function App() {
-  return <div />;
+  return <Header />;
 }
 
 export default App;
