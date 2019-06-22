e964f9376d711c4b1d3ba2209dfa00d8a2c82a3a
AbdelrahmanSE <abdelrahmanse@flexnebula.com>
2019-06-23 01:07:20 +0200

    Added React Router and integrated it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+
 import "./App.module.scss";
-import Layout from './hoc/Layout/Layout';
+import Layout from "./hoc/Layout/Layout";
+import Rooms from "./containers/Rooms/Rooms";
 
 function App() {
   return (
     <Layout>
-      
+      <Switch>
+        <Route path="/" exact component={Rooms} />
+      </Switch>
     </Layout>
   );
 }
---
+++ src/containers/Rooms/Rooms.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Rooms extends Component {
+  render() {
+    return <div>Rooms</div>;
+  }
+}
