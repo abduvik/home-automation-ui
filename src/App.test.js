261370696298b785cfee9fc15f8534a94eae3052
AbdelrahmanSE <abdelrahmanse@flexnebula.com>
2019-06-22 17:50:31 +0200

    Added testing packages and create first <App /> test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { configure, shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({ adapter: new EnzymeAdapter() });
+
+describe("<App />", () => {
+    
+  it("renders without crashing", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).toBeTruthy();
+  });
 });
