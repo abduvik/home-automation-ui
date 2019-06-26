5cd7bf526552421f620b7fab99b0ffa7f5570927
AbdelrahmanSE <abdelrahmanse@flexnebula.com>
2019-06-27 01:36:36 +0200

    Created Scale Component + Unit tests

--- src/components/Device/Controls/Scale/ScaleControl.js
+++ src/components/Device/Controls/Scale/ScaleControl.js
@@ -1,17 +1,37 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-function ScaleControl(props) {
+import classes from "./ScaleControl.module.scss";
+
+export default class ScaleControl extends Component {
+  static propTypes = {
+    controlId: PropTypes.string,
+    name: PropTypes.string,
+    type: PropTypes.string,
+    value: PropTypes.number,
+    min: PropTypes.number,
+    max: PropTypes.number,
+    step: PropTypes.number
+  };
+
+  onChangeScaleHandler = event => {
+    const updatedValue = event.target.value;
+    if (updatedValue !== this.props.value) {
+      this.props.onUpdateValue(this.props.controlId, parseInt(updatedValue));
+    }
+  };
+
+  render() {
     return (
-        <div>
-            Scale
-        </div>
-    )
+      <input
+        className={classes.Range}
+        type="range"
+        min={this.props.min}
+        max={this.props.max}
+        step={this.props.step}
+        value={this.props.value}
+        onChange={this.onChangeScaleHandler}
+      />
+    );
+  }
 }
-
-ScaleControl.propTypes = {
-
-}
-
-export default ScaleControl
-
--- src/components/Device/Controls/Scale/ScaleControl.test.js
+++ src/components/Device/Controls/Scale/ScaleControl.test.js
@@ -1,11 +1,36 @@
 import React from "react";
 import { shallow } from "enzyme";
-import ScaleControl from './ScaleControl';
+import ScaleControl from "./ScaleControl";
 
 describe("<ScaleControl />", () => {
-    
+  const scaleControlDummyProps = {
+    controlId: "2412412412",
+    name: "Intensity",
+    type: "scale",
+    value: 20,
+    min: 0,
+    max: 100,
+    step: 10
+  };
+
   it("renders without crashing", () => {
     const wrapper = shallow(<ScaleControl />);
     expect(wrapper).toBeTruthy();
+  });
+
+  it("should call onUpdateValue when the new value is not the same as current value", () => {
+    const onUpdateValue = jest.fn();
+    const wrapper = shallow(
+      <ScaleControl {...scaleControlDummyProps} onUpdateValue={onUpdateValue} />
+    );
+
+    wrapper
+      .find("input[type='range']")
+      .simulate("change", { target: { value: 30 } });
+      
+    expect(onUpdateValue).toHaveBeenCalledWith(
+      scaleControlDummyProps.controlId,
+      30
+    );
   });
 });
