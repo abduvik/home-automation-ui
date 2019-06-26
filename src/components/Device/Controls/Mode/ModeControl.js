ecb1df3f975379e509a19354b4e480e0eb8feb4f
AbdelrahmanSE <abdelrahmanse@flexnebula.com>
2019-06-27 00:41:42 +0200

    Created Mode control + Unit tests + fix for Dummy Data

--- src/components/Device/Controls/Mode/ModeControl.js
+++ src/components/Device/Controls/Mode/ModeControl.js
@@ -1,17 +1,55 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function ModeControl(props) {
-    return (
-        <div>
-            Mode
+import classes from "./ModeControl.module.scss";
+
+export default class ModeControl extends Component {
+  static propTypes = {
+    controlId: PropTypes.string,
+    name: PropTypes.string,
+    value: PropTypes.string,
+    onUpdateValue: PropTypes.func,
+    options: PropTypes.object
+  };
+
+  onChangeModeHandler = event => {
+    const updatedValue = event.target.value;
+    if (updatedValue !== this.props.value) {
+      this.props.onUpdateValue(this.props.controlId, updatedValue);
+    }
+  };
+
+  render() {
+    if (!this.props.options) return null;
+
+    const options = Object.entries(this.props.options).map(optionData => {
+      const openKey = optionData[0];
+      const option = optionData[1];
+
+      const isChecked = this.props.value === openKey;
+      const radioId = `${this.props.controlId}-${openKey}`;
+
+      return (
+        <div className={classes.ModeControl} key={openKey}>
+          <input
+            type="radio"
+            id={radioId}
+            value={openKey}
+            className={classes.Radio}
+            checked={isChecked}
+            onChange={this.onChangeModeHandler}
+          />
+          <label htmlFor={radioId} className={classes.Label}>
+            {option.icon ? (
+              <FontAwesomeIcon icon={{ prefix: "fa", iconName: option.icon }} />
+            ) : (
+              <span className={classes.OptionText}>{option.name}</span>
+            )}
+          </label>
         </div>
-    )
+      );
+    });
+    return <div className={classes.ModeControlContainer}>{options}</div>;
+  }
 }
-
-ModeControl.propTypes = {
-    
-}
-
-export default ModeControl
-
--- src/components/Device/Controls/Mode/ModeControl.test.js
+++ src/components/Device/Controls/Mode/ModeControl.test.js
@@ -1,11 +1,79 @@
 import React from "react";
 import { shallow } from "enzyme";
-import ModeControl from './ModeControl';
+import ModeControl from "./ModeControl";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 describe("<ModeControl />", () => {
-    
+  const modeControlDummyProps = {
+    controlId: "3368059519",
+    name: "Intensity",
+    type: "mode",
+    value: "medium"
+  };
+
+  const textOnlyOptions = {
+    low: {
+      name: "Low",
+      icon: ""
+    },
+    medium: {
+      name: "Medium",
+      icon: ""
+    },
+    high: {
+      name: "High",
+      icon: ""
+    }
+  };
+
+  const iconOptions = {
+    low: {
+      name: "Low",
+      icon: "couch"
+    },
+    medium: {
+      name: "Medium",
+      icon: "couch"
+    },
+    high: {
+      name: "High",
+      icon: "couch"
+    }
+  };
+
   it("renders without crashing", () => {
     const wrapper = shallow(<ModeControl />);
     expect(wrapper).toBeTruthy();
+  });
+
+  it("should render text-only option if it has no image", () => {
+    modeControlDummyProps.options = textOnlyOptions;
+    const wrapper = shallow(<ModeControl {...modeControlDummyProps} />);
+    expect(wrapper.find("label span")).toHaveLength(3);
+  });
+
+  it("should render a <FontAwesomeIcon /> if the option have an icon for it", () => {
+    modeControlDummyProps.options = iconOptions;
+    const wrapper = shallow(<ModeControl {...modeControlDummyProps} />);
+    expect(wrapper.find(FontAwesomeIcon)).toHaveLength(3);
+  });
+
+  it("should call onUpdateValue when the clicked option is not the same as current value", () => {
+    const onUpdateValue = jest.fn();
+    modeControlDummyProps.options = iconOptions;
+    const wrapper = shallow(
+      <ModeControl {...modeControlDummyProps} onUpdateValue={onUpdateValue} />
+    );
+
+    // Clicked the first option
+    wrapper
+      .find("input[type='radio']")
+      .first()
+      .simulate("change", {target:{value: 'low'}});
+
+    expect(onUpdateValue).toHaveBeenCalledWith(
+      modeControlDummyProps.controlId,
+      "low"
+    );
   });
 });
